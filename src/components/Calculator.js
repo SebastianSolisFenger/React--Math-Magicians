3d266ceccaf304f734c9356a530c2303fab1bdf2
Sebastian Daniel Solis Fenger <solisfenger@gmail.com>
2022-05-24 17:02:33 -0300

    make it KISS

--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -6,10 +6,10 @@ class Calculator extends React.Component {
   constructor(props) {
     super(props);
     this.state = { total: 0, next: null, operation: null };
-    this.handleClick = this.handleClick.bind(this);
+    this.clickBtn = this.clickBtn.bind(this);
   }
 
-  handleClick(e) {
+  clickBtn(e) {
     this.setState((state) => calculate(state, e.target.id));
   }
 
@@ -24,7 +24,7 @@ class Calculator extends React.Component {
         </h2>
         <button
           type="button"
-          onClick={this.handleClick}
+          onClick={this.clickBtn}
           id="AC"
           className="box bt"
         >
@@ -32,119 +32,69 @@ class Calculator extends React.Component {
         </button>
         <button
           type="button"
-          onClick={this.handleClick}
+          onClick={this.clickBtn}
           id="+/-"
           className="box bt"
         >
           +/-
         </button>
-        <button
-          type="button"
-          onClick={this.handleClick}
-          id="%"
-          className="box bt"
-        >
+        <button type="button" onClick={this.clickBtn} id="%" className="box bt">
           %
         </button>
         <button
           type="button"
-          onClick={this.handleClick}
+          onClick={this.clickBtn}
           id="÷"
           className="box bg-orange bt"
         >
           ÷
         </button>
-        <button
-          type="button"
-          onClick={this.handleClick}
-          id="7"
-          className="box bt"
-        >
+        <button type="button" onClick={this.clickBtn} id="7" className="box bt">
           7
         </button>
-        <button
-          type="button"
-          onClick={this.handleClick}
-          id="8"
-          className="box bt"
-        >
+        <button type="button" onClick={this.clickBtn} id="8" className="box bt">
           8
         </button>
-        <button
-          type="button"
-          onClick={this.handleClick}
-          id="9"
-          className="box bt"
-        >
+        <button type="button" onClick={this.clickBtn} id="9" className="box bt">
           9
         </button>
         <button
           type="button"
-          onClick={this.handleClick}
+          onClick={this.clickBtn}
           id="x"
           className="box bg-orange bt"
         >
           x
         </button>
-        <button
-          type="button"
-          onClick={this.handleClick}
-          id="4"
-          className="box bt"
-        >
+        <button type="button" onClick={this.clickBtn} id="4" className="box bt">
           4
         </button>
-        <button
-          type="button"
-          onClick={this.handleClick}
-          id="5"
-          className="box bt"
-        >
+        <button type="button" onClick={this.clickBtn} id="5" className="box bt">
           5
         </button>
-        <button
-          type="button"
-          onClick={this.handleClick}
-          id="6"
-          className="box bt"
-        >
+        <button type="button" onClick={this.clickBtn} id="6" className="box bt">
           6
         </button>
         <button
           type="button"
-          onClick={this.handleClick}
+          onClick={this.clickBtn}
           id="-"
           className="box bg-orange bt"
         >
           -
         </button>
-        <button
-          type="button"
-          onClick={this.handleClick}
-          id="1"
-          className="box bt"
-        >
+        <button type="button" onClick={this.clickBtn} id="1" className="box bt">
           1
         </button>
-        <button
-          type="button"
-          onClick={this.handleClick}
-          id="2"
-          className="box bt"
-        >
+        <button type="button" onClick={this.clickBtn} id="2" className="box bt">
           2
         </button>
-        <button
-          type="button"
-          onClick={this.handleClick}
-          id="3"
-          className="box bt"
-        >
+        <button type="button" onClick={this.clickBtn} id="3" className="box bt">
           3
         </button>
         <button
           type="button"
-          onClick={this.handleClick}
+          onClick={this.clickBtn}
           id="+"
           className="box bg-orange bt"
         >
@@ -152,23 +102,18 @@ class Calculator extends React.Component {
         </button>
         <button
           type="button"
-          onClick={this.handleClick}
+          onClick={this.clickBtn}
           id="0"
           className="box zero bt"
         >
           0
         </button>
-        <button
-          type="button"
-          onClick={this.handleClick}
-          id="."
-          className="box bt"
-        >
+        <button type="button" onClick={this.clickBtn} id="." className="box bt">
           .
         </button>
         <button
           type="button"
-          onClick={this.handleClick}
+          onClick={this.clickBtn}
           id="="
           className="box bg-orange bt"
         >
