e36bc250251a1b5f68784eea558759a3d5ecfc16
Sebastian Daniel Solis Fenger <solisfenger@gmail.com>
2022-05-31 08:58:24 -0300

    Added files and coded Home page html structure

---
+++ src/components/Home.js
@@ -0,0 +1,33 @@
+import React from 'react';
+
+const Home = () => (
+  <div className="container">
+    <h1> Welcome to Math Magicians!</h1>
+    <p>
+      Lorem ipsum dolor sit, amet consectetur adipisicing elit. Numquam vitae,
+      dolore minus amet laborum repellendus quia, at omnis aperiam cupiditate
+      sunt. Maxime voluptatem aliquid tempore nam natus eaque pariatur expedita!
+      Laboriosam tempora, illum quae dolorem amet tenetur cum ratione
+      architecto, nihil iste voluptas facere error! Ex architecto quasi omnis
+      harum laudantium eligendi ipsam perspiciatis dolor quae, ipsum a dicta
+      corrupti! Ipsam, doloremque, cum culpa aperiam enim perferendis labore
+      quae consectetur id magnam sit ipsum deserunt, quas provident earum.
+      Similique quas quo itaque! Laudantium inventore hic atque cum, placeat
+      nemo illum.
+    </p>
+    <p>
+      Lorem ipsum dolor sit, amet consectetur adipisicing elit. Numquam vitae,
+      dolore minus amet laborum repellendus quia, at omnis aperiam cupiditate
+      sunt. Maxime voluptatem aliquid tempore nam natus eaque pariatur expedita!
+      Laboriosam tempora, illum quae dolorem amet tenetur cum ratione
+      architecto, nihil iste voluptas facere error! Ex architecto quasi omnis
+      harum laudantium eligendi ipsam perspiciatis dolor quae, ipsum a dicta
+      corrupti! Ipsam, doloremque, cum culpa aperiam enim perferendis labore
+      quae consectetur id magnam sit ipsum deserunt, quas provident earum.
+      Similique quas quo itaque! Laudantium inventore hic atque cum, placeat
+      nemo illum.
+    </p>
+  </div>
+);
+
+export default Home;
