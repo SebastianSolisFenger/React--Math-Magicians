04debabeb75d9ba0bd34bec2b23336b26e7ecac8
Sebastian Daniel Solis Fenger <solisfenger@gmail.com>
2022-05-31 13:10:09 -0300

    Add final styling touches and fix routing bugs

--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -10,94 +10,102 @@ const Calculator = () => {
 
   const { total, next, operation } = obj;
   return (
-    <div className="calculator-Container">
-      <h2 className="defaultOutput box">
-        <span>{total}</span>
-        <span>{operation}</span>
-        <span>{next}</span>
-      </h2>
-      <button type="button" onClick={clickBtn} id="AC" className="box btn">
-        AC
-      </button>
-      <button type="button" onClick={clickBtn} id="+/-" className="box btn">
-        +/-
-      </button>
-      <button type="button" onClick={clickBtn} id="%" className="box btn">
-        %
-      </button>
-      <button
-        type="button"
-        onClick={clickBtn}
-        id="÷"
-        className="box bg-orange btn"
-      >
-        ÷
-      </button>
-      <button type="button" onClick={clickBtn} id="7" className="box btn">
-        7
-      </button>
-      <button type="button" onClick={clickBtn} id="8" className="box btn">
-        8
-      </button>
-      <button type="button" onClick={clickBtn} id="9" className="box btn">
-        9
-      </button>
-      <button
-        type="button"
-        onClick={clickBtn}
-        id="x"
-        className="box bg-orange bt"
-      >
-        x
-      </button>
-      <button type="button" onClick={clickBtn} id="4" className="box btn">
-        4
-      </button>
-      <button type="button" onClick={clickBtn} id="5" className="box btn">
-        5
-      </button>
-      <button type="button" onClick={clickBtn} id="6" className="box btn">
-        6
-      </button>
-      <button
-        type="button"
-        onClick={clickBtn}
-        id="-"
-        className="box bg-orange btn"
-      >
-        -
-      </button>
-      <button type="button" onClick={clickBtn} id="1" className="box btn">
-        1
-      </button>
-      <button type="button" onClick={clickBtn} id="2" className="box btn">
-        2
-      </button>
-      <button type="button" onClick={clickBtn} id="3" className="box btn">
-        3
-      </button>
-      <button
-        type="button"
-        onClick={clickBtn}
-        id="+"
-        className="box bg-orange btn"
-      >
-        +
-      </button>
-      <button type="button" onClick={clickBtn} id="0" className="box zero btn">
-        0
-      </button>
-      <button type="button" onClick={clickBtn} id="." className="box btn">
-        .
-      </button>
-      <button
-        type="button"
-        onClick={clickBtn}
-        id="="
-        className="box bg-orange btn"
-      >
-        =
-      </button>
+    <div className="calculator-Container ">
+      <h1 className="letsDo">Let&apos;s Do Some Math</h1>
+      <div className="app">
+        <h2 className="defaultOutput box">
+          <span>{total}</span>
+          <span>{operation}</span>
+          <span>{next}</span>
+        </h2>
+        <button type="button" onClick={clickBtn} id="AC" className="box btn">
+          AC
+        </button>
+        <button type="button" onClick={clickBtn} id="+/-" className="box btn">
+          +/-
+        </button>
+        <button type="button" onClick={clickBtn} id="%" className="box btn">
+          %
+        </button>
+        <button
+          type="button"
+          onClick={clickBtn}
+          id="÷"
+          className="box bg-orange btn"
+        >
+          ÷
+        </button>
+        <button type="button" onClick={clickBtn} id="7" className="box btn">
+          7
+        </button>
+        <button type="button" onClick={clickBtn} id="8" className="box btn">
+          8
+        </button>
+        <button type="button" onClick={clickBtn} id="9" className="box btn">
+          9
+        </button>
+        <button
+          type="button"
+          onClick={clickBtn}
+          id="x"
+          className="box bg-orange bt"
+        >
+          x
+        </button>
+        <button type="button" onClick={clickBtn} id="4" className="box btn">
+          4
+        </button>
+        <button type="button" onClick={clickBtn} id="5" className="box btn">
+          5
+        </button>
+        <button type="button" onClick={clickBtn} id="6" className="box btn">
+          6
+        </button>
+        <button
+          type="button"
+          onClick={clickBtn}
+          id="-"
+          className="box bg-orange btn"
+        >
+          -
+        </button>
+        <button type="button" onClick={clickBtn} id="1" className="box btn">
+          1
+        </button>
+        <button type="button" onClick={clickBtn} id="2" className="box btn">
+          2
+        </button>
+        <button type="button" onClick={clickBtn} id="3" className="box btn">
+          3
+        </button>
+        <button
+          type="button"
+          onClick={clickBtn}
+          id="+"
+          className="box bg-orange btn"
+        >
+          +
+        </button>
+        <button
+          type="button"
+          onClick={clickBtn}
+          id="0"
+          className="box zero btn"
+        >
+          0
+        </button>
+        <button type="button" onClick={clickBtn} id="." className="box btn">
+          .
+        </button>
+        <button
+          type="button"
+          onClick={clickBtn}
+          id="="
+          className="box bg-orange btn"
+        >
+          =
+        </button>
+      </div>
     </div>
   );
 };
--- src/components/Quote.js
+++ src/components/Quote.js
@@ -2,19 +2,11 @@ import React from 'react';
 
 const Quote = () => (
   <div className="container">
-    <h2>
-      Lorem ipsum dolor sit amet, consectetur adipisicing elit. Atque
-      repellendus temporibus minima iure quaerat deleniti deserunt quos,
-      aliquam, necessitatibus illo ipsam quam perspiciatis dolor iusto. Ut
-      delectus earum eaque blanditiis! Vel praesentium odit repudiandae natus
-      hic debitis, modi dolores officiis molestiae obcaecati deleniti illo
-      neque, cum recusandae ab inventore nostrum autem odio libero iusto quas?
-      Mollitia corporis voluptates quod eveniet! Vero, corporis repellendus
-      maiores, alias dolor eaque iste labore nobis quas nulla quia atque eum
-      voluptate molestiae porro possimus expedita aliquid neque modi id in. Vel
-      quod animi magnam inventore!
-      <span className="authorname"> -- by XXXX XXXXXXXX</span>
+    <h2 className="Quote-P">
+      Mathematics is not about numbers, equations, computations, or algorithms:
+      it is about understanding.
     </h2>
+    <span className="authorname"> -- by William Paul Thuston</span>
   </div>
 );
 
