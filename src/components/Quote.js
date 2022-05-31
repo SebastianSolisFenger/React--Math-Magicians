dfbff6412d3ca56ac310be130d4c04f4f054ecda
Sebastian Daniel Solis Fenger <solisfenger@gmail.com>
2022-05-31 09:13:40 -0300

    Add quote.js structure

---
+++ src/components/Quote.js
@@ -0,0 +1,21 @@
+import React from 'react';
+
+const Quote = () => (
+  <div className="container">
+    <h2>
+      Lorem ipsum dolor sit amet, consectetur adipisicing elit. Atque
+      repellendus temporibus minima iure quaerat deleniti deserunt quos,
+      aliquam, necessitatibus illo ipsam quam perspiciatis dolor iusto. Ut
+      delectus earum eaque blanditiis! Vel praesentium odit repudiandae natus
+      hic debitis, modi dolores officiis molestiae obcaecati deleniti illo
+      neque, cum recusandae ab inventore nostrum autem odio libero iusto quas?
+      Mollitia corporis voluptates quod eveniet! Vero, corporis repellendus
+      maiores, alias dolor eaque iste labore nobis quas nulla quia atque eum
+      voluptate molestiae porro possimus expedita aliquid neque modi id in. Vel
+      quod animi magnam inventore!
+      <span className="authorname"> -- by XXXX XXXXXXXX</span>
+    </h2>
+  </div>
+);
+
+export default Quote;
